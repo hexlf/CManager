33491ba7485632e57571be3db79d575c4ef37bb1
ZelFS <86790952+hexlf@users.noreply.github.com>
2026-01-01 14:25:13 +0100

    Made WhenCustomerIsCreatedSuccessfully to test Customer creation

--- CManager.Test/ServiceTest/CustomerServiceTest.cs
+++ CManager.Test/ServiceTest/CustomerServiceTest.cs
@@ -1,20 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿using CManager.App.Services;
+using CManager.dom.Models;
+using CManager.repo.Repos;
+using Moq;
 
 namespace CManager.Test.ServiceTest;
 
 public class CustomerServiceTest
 {
+
     [Fact]
-    public void TestMethod1()
+    public void CreateCustomer_ShouldReturnTrue_WhenCustomerIsCreatedSuccessfully()
     {
         // Arrange
+        var mockCustomerRepo = new Mock<ICustomerRepo>();
+        mockCustomerRepo.Setup(repo => repo.GetCustomers()).Returns(new List<CustomerModel>());
+        mockCustomerRepo.Setup(repo => repo.SaveCustomerModel(It.IsAny<List<CustomerModel>>())).Returns(true);
+        var customerService = new CustomerService(mockCustomerRepo.Object);
 
         // Act
+        var result = customerService.CreateCustomer("aaa", "eee", "em@gmail.com", "1234567890", "123", "12345", "Cccc");
 
         // Assert
-
+        Assert.True(result);
     }
+
 }
+
